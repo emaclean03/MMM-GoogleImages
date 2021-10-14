3e17a0f2671dbbd2ebb9ac095207522a7ac35146
Ian <Ianidane@users.noreply.github.com>
2021-10-14 08:08:15 -0400

    Update node_helper.js
    Photo URL to env

--- node_helper.js
+++ node_helper.js
@@ -8,7 +8,7 @@ module.exports = NodeHelper.create({
         /*Sends the payload back to broadcast to clients*/
         var self = this;
 
-        request('https://photos.google.com/share/AF1QipNyFDQqs2z9T8_pC-SDuqWDobnHaniKoZLys2CPzF_BiqxkK65OyQO-73NGqsbuAQ?key=U2FMOFNNU25xOTBCR3B1eGxzeldJaHlMRC1fZEh3', function(error, response, body){
+        request(process.env.PHOTO_URL, function(error, response, body){
             self.sendSocketNotification('Image', extractPhotos(body))
         });
 
